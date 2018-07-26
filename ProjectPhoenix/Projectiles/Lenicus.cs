c7642753ee408961b50c448e7fa0e763395a338d
Astrum76 <41600425+Astrum76@users.noreply.github.com>
2018-07-26 17:50:36 -0400

    Add files via upload

--- ProjectPhoenix/Projectiles/Lenicus.cs
+++ ProjectPhoenix/Projectiles/Lenicus.cs
@@ -12,6 +12,8 @@ namespace ProjectPhoenix.Projectiles
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Lenicus");
+			Main.projFrames[projectile.type] = 7; //This is an animated projectile
+
 		}
 		public override void SetDefaults()
 		{
@@ -19,40 +21,34 @@ namespace ProjectPhoenix.Projectiles
 			projectile.height = 14;
             projectile.friendly = true;
             projectile.penetrate = -1;                       //this is the projectile penetration
-            Main.projFrames[projectile.type] = 56;           //this is projectile frames
             projectile.hostile = false;
 			projectile.magic = true;                        //this make the projectile do magic damage
             projectile.tileCollide = true;                 //this make that the projectile does not go thru walls
 			projectile.ignoreWater = true;
 		}
 
-		public override void AI()
-		{
+		 public override void AI()
+        {
                                                                 //this is projectile dust
             int DustID2 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y + 2f), projectile.width + 2, projectile.height + 2, mod.DustType("DustName"), projectile.velocity.X * 0.2f, projectile.velocity.Y * 0.2f, 20, default(Color), 2.9f);
             Main.dust[DustID2].noGravity = true;
-                                                          //this make that the projectile faces the right way 
+                                                          //this make that the projectile faces the right way
             projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
             projectile.localAI[0] += 1f;
-			projectile.alpha = (int)projectile.localAI[0] * 2;
-			
-			if (projectile.localAI[0] > 130f) //projectile time left before disappears
+            projectile.alpha = (int)projectile.localAI[0] * 2;
+          
+            if (projectile.localAI[0] > 130f) //projectile time left before disappears
             {
-				projectile.Kill();
-			}
-           
-        }
-        public override bool PreDraw(SpriteBatch sb, Color lightColor) //this is where the animation happens
-        {
-            projectile.frameCounter++; //increase the frameCounter by one
-            if (projectile.frameCounter >= 20) //once the frameCounter has reached 10 - change the 10 to change how fast the projectile animates
-            {
-                projectile.frame++; //go to the next frame
-                projectile.frameCounter = 0; //reset the counter
-                if (projectile.frame > 55) //if past the last frame
-                    projectile.frame = 0; //go back to the first frame
+                projectile.Kill();
             }
-            return true;
+                       // Loop through the 56 animation frames, spending 15 ticks on each.
+            if (++projectile.frameCounter >= 15)
+            {
+                projectile.frameCounter = 0;
+                if (++projectile.frame >= 56)
+                {
+                    projectile.frame = 0;
+                }
+            }
         }
-    }
-}
+		
